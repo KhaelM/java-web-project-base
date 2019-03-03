ff71006f73517d0e04408406c2b572830a12c9b7
KhaelM <mikerandria@gmail.com>
2019-03-03 15:11:50 -0500

    formatDate moved to TimeUtility

--- WEB-INF/src/com/michael/database/GeneralizedFunction.java
+++ WEB-INF/src/com/michael/database/GeneralizedFunction.java
@@ -33,31 +33,6 @@ public class GeneralizedFunction {
         }
     }
 
-    public static String formatDate(String date) throws Exception {
-        /**
-         * Format accepted:
-         * YYYY-MM-DD => ENG format
-         * DD-MM-YYYY => FR format
-         * 
-         */
-        // Let's check if it contains a Date first
-        String regexENG = "([0-9]{4}).([0-9]{2}).([0-9]{2})( )?([0-9]{2}.[0-9]{2}.[0-9]{2})?";
-        String regexFR = "([0-9]{2}).([0-9]{2}).([0-9]{4})( )?([0-9]{2}.[0-9]{2}.[0-9]{2})?";
-        String replacement = null;
-
-        if(!date.matches(regexENG) && !date.matches(regexFR)) {
-            return date;
-        }
-        
-        if(date.matches(regexENG)) {
-            replacement = "$1-$2-$3$4$5";
-            return date.replaceAll(regexENG, replacement);
-        } else {
-            replacement = "$3-$2-$1$4$5";
-            return date.replaceAll(regexFR, replacement);
-        }
-    }
-
     public static List<Field> getAllFields(Class<?> type) {
         List<Field> fields = new ArrayList<Field>();
         for (Class<?> c = type; c != null; c = c.getSuperclass()) {
