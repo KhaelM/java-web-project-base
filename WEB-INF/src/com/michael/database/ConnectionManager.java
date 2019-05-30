8d4cb8cfef57919ece8124976e687e6774e393f6
KhaelM <mikerandria@gmail.com>
2019-05-30 06:52:13 +0300

    postgresql jdbc added

--- WEB-INF/src/com/michael/database/ConnectionManager.java
+++ WEB-INF/src/com/michael/database/ConnectionManager.java
@@ -1,6 +1,7 @@
 package com.michael.database;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConnectionManager{
     public static Connection getConnection(String url, String driverName, String username, String password) throws Exception {
@@ -17,5 +18,9 @@ public class ConnectionManager{
 
     public static Connection getOracleConnection(String username, String password) throws Exception {
         return getConnection("jdbc:oracle:thin:@localhost:1521:xe", "oracle.jdbc.driver.OracleDriver", username, password);
+    }
+
+    public static Connection getPsqlConnection(String username, String password) throws Exception {
+        return getConnection("jdbc:postgresql://127.0.0.1:5432/michael", "org.postgresql.Driver", username, password);
     }
 }
